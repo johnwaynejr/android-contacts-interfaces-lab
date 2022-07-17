fbc67239ccf523d3f85f8793dede548647ceb801
johnwaynejr <navuhdonosor11@gmail.com>
2022-07-17 11:05:47 +0300

    Интерфейсы.Практика. Создан и реализован интерфейс "OnDebounceListener".

--- app/src/main/java/ru/yandex/practicum/contacts/utils/android/Debouncer.java
+++ app/src/main/java/ru/yandex/practicum/contacts/utils/android/Debouncer.java
@@ -12,11 +12,11 @@ public class Debouncer {
 
     private static final int MESSAGE_ID = 1;
     private static final int DELAY = 500;
+   // private final MainViewModel viewModel;
+    private final OnDebounceListener listener;
 
-    private final MainViewModel viewModel;
-
-    public Debouncer(MainViewModel viewModel) {
-        this.viewModel = viewModel;
+    public Debouncer(OnDebounceListener listener) {
+        this.listener = listener;
     }
 
     private final Handler handler = new Handler(Looper.getMainLooper()) {
@@ -39,6 +39,8 @@ public class Debouncer {
 
     // выполнить действие по прошествии 500мс, если нового события в течении 500мс не было отправлено
     private void doOnDebounce() {
-        viewModel.search();
+        listener.onDebounce();
     }
+
+
 }
